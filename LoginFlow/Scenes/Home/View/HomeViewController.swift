04f44fa05926cc0f94f6ee1d9ba0e7a08135f3d6
Jaewon <woin2ee@gmail.com>
2022-07-30 18:29:03 +0900

    feat : Keychain 읽어오는 함수 프로토타입 구현

--- LoginFlow/Scenes/Home/View/HomeViewController.swift
+++ LoginFlow/Scenes/Home/View/HomeViewController.swift
@@ -15,10 +15,32 @@ final class HomeViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear >>>>> \(readKeychain())")
+    }
+    
     @IBAction func didTapLoginButton(_ sender: UIBarButtonItem) {
         let storyboard = UIStoryboard.init(name: "Login", bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: String(describing: LoginViewController.self))
         vc.modalPresentationStyle = .fullScreen
         self.present(vc, animated: false)
+    }
+    
+    private func readKeychain() -> String {
+        let query: [CFString : Any] = [kSecClass : kSecClassGenericPassword,
+                                 kSecAttrService : "Service name",
+                                 kSecAttrAccount : "Account",
+                                  kSecMatchLimit : kSecMatchLimitOne,
+                            kSecReturnAttributes : true,
+                                  kSecReturnData : true]
+        var item: CFTypeRef?
+        if SecItemCopyMatching(query as CFDictionary, &item) != errSecSuccess { return "none" }
+        
+        guard let existingItem = item as? [CFString : Any],
+              let token = existingItem[kSecAttrGeneric] as? String
+        else { return "none" }
+        
+        return token
     }
 }
